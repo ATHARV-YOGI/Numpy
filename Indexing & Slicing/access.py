133ff9d0c4073720b183a61a3ae969ac974d631a
Atharv Yogi <atharvyogi123@gmail.com>
2025-08-29 09:27:52 +0530

    commit

---
+++ Indexing & Slicing/access.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+arr = np.array([1,2,3,4,5])
+
+
+# print using indexing 
+print(arr[0])
+print(arr[2])
+
+print(arr[-1])  #print last element
+
+# print using slicing
+
+print(arr[0:5]) #print 0 to 4
+
+print(arr[:5]) #print 0 to 4
+
+print(arr[::2])  #print every second element
